34711ac323dde3a84f90b35141e4b3f843f22020
huangjiaqian <18775134221@163.com>
2018-09-04 17:41:15 +0800

    完成选择城市页面搜索框

--- vue-travel/src/components/home/city/City.vue
+++ vue-travel/src/components/home/city/City.vue
@@ -1,16 +1,21 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 
 export default {
   name: 'City',
-  components: {CityHeader},
+  components: {CitySearch, CityHeader},
   comments: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ vue-travel/src/components/home/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市的名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/index.styl"
+  .search
+    overflow hidden
+    height .64rem
+    line-height .64rem
+    padding 0 .1rem
+    background-color $themecolor
+    .search-input
+      box-sizing border-box
+      color #666
+      width 100%
+      text-align center
+      padding 0 .1rem
+      border-radius .03rem
+</style>
