8432f5e3f411df1bc578456ad9842b19099ce566
huangjiaqian <18775134221@163.com>
2018-09-04 20:17:32 +0800

    完成城市选择页面搭建

---
+++ vue-travel/src/components/home/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="list">
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/index.styl"
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.52rem
+    bottom 0
+    right 0
+    width .4rem
+    .item
+      text-align center
+      line-height .44rem
+      color $themecolor
+</style>
---
+++ vue-travel/src/components/home/city/components/List.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div class="content">
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">南宁市</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">南宁市</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">南宁市</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">南宁市</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">南宁市</div>
+          </div>
+        </div>
+      </div>
+      <div class="list-item">
+        <div class="item">
+          <div class="title border-topbottom">A</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+        </div>
+        <div class="item">
+          <div class="title border-topbottom">B</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+        </div>
+        <div class="item">
+          <div class="title border-topbottom">C</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+          <div class="item-name border-bottom">南宁市</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/index.styl"
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    left 0
+    top 1.52rem
+    right 0
+    bottom 0
+    .title
+      line-height .4rem
+      padding-left .2rem
+      color #666
+      background-color #eee
+      font-size .26rem
+    .button-list
+      overflow hidden
+      padding .1rem .6rem .1rem .1rem
+      .button-wrapper
+        overflow hidden
+        float left
+        width 33.33%
+        .button
+          margin .1rem
+          padding .1rem 0
+          text-align center
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-name
+      line-height .70rem
+      padding-left .2rem
+</style>
